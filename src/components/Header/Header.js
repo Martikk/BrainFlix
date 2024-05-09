1a57fec95388e41bee4aad8de9ad060d3e7cb8ba
your_name <alex.martishyn@gmail.com>
2024-05-09 00:32:16 -0700

    add_Upload_Page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Header.scss';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../../assets/Logo/BrainFlix-logo.svg';
 import icon from '../../assets/Images/Mohan-muruge.jpg';
 import Search from '../Search/Search'; // Import the Search component
 
 function Header({ onSearch, searchResults, onSelectVideo }) {
+    const navigate = useNavigate(); // Create the navigate function
+
+    // Function to handle click event on Upload button
+    const handleUploadClick = () => {
+        navigate('/UploadVideo'); // Navigate to the UploadVideo page
+    };
     return (
         <header className="header">
             <Link className="header__logo" to="/">
@@ -13,13 +19,14 @@ function Header({ onSearch, searchResults, onSelectVideo }) {
             </Link>
             <Search onSearch={onSearch} searchResults={searchResults} onSelectVideo={onSelectVideo} />
             <div className="header__actions">
-                <button className="header__upload">Upload</button>
+                <button className="header__upload" onClick={handleUploadClick}>Upload</button>
             </div>
             <div className="header__user-icon">
-                <img src={icon} alt="ALex Martishyn"/>
+            <img src={icon} alt="ALex Martishyn"/>
             </div>
         </header>
-    );
+);
 }
+
 
 export default Header;
